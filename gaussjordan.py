59ccb93ef49cd9d6b630ae074390c38207d4ee47
adieos <athanokwibisono@gmail.com>
2023-09-06 15:27:24 +0700

    refactor: put main code inside main() func

--- gaussjordan.py
+++ gaussjordan.py
@@ -15,47 +15,54 @@ def oneify(arr1: list, index:int):
     divisor = float(arr1[index])
     return (arr1 / divisor) if divisor else arr1
 
-if __name__ == "__main__":
+def main():
+    running =  True
     print("Hello! This little tool will help you solve a linear algebra question using Gauss-Jordan Elimination.")
+    while running:
 
-    equations = []
-    coefficients = []
+        equations = []
+        coefficients = []
 
-    # ask the user for equations
-    try:
-        for n in range(3):
-            eq = input(f"Enter equation #{n+1}: ").replace(" ","")
-            equations.append(eq)
-            coeff = [int(i) for i in re.findall("(\d+|-\d+)", eq)]
-            if len(coeff) != 4:
-                raise InvalidEquation("At least one of your equation is invalid!")
-            coefficients.append(coeff)
-    except InvalidEquation as e:
-        print(e.message)
-        os.system("pause")
-        exit(1)
+        # ask the user for equations
+        try:
+            for n in range(3):
+                eq = input(f"Enter equation #{n+1}: ").replace(" ","")
+                equations.append(eq)
+                coeff = [int(i) for i in re.findall("(\d+|-\d+)", eq)]
+                if len(coeff) != 4:
+                    raise InvalidEquation("At least one of your equation is invalid! Terminating program NOW")
+                coefficients.append(coeff)
+        except InvalidEquation as e:
+            print(e.message)
+            os.system("pause")
+            exit(1)
 
-    matrix = np.array(coefficients, dtype=float)
-    print("Initial matrix:")
-    print(matrix)
+        matrix = np.array(coefficients, dtype=float)
+        print("Initial matrix:")
+        print(matrix)
 
-    iteration = 1
-    # iteration 1 - 6
-    for i in range(len(matrix)):
-        matrix[i] = oneify(matrix[i], i)
-        print(f"Iteration #{iteration}: \n{matrix}\n")
-        iteration += 1
-
-        for j in range(i+1, len(matrix)):
-            matrix[j] = zeroify(matrix[i], matrix[j], i)
+        iteration = 1
+        # iteration 1 - 6
+        for i in range(len(matrix)):
+            matrix[i] = oneify(matrix[i], i)
             print(f"Iteration #{iteration}: \n{matrix}\n")
             iteration += 1
 
-    # iteration 7 - 9
-    for i in range(len(matrix) - 1, 0, -1):
-        for j in range(i-1, -1, -1):
-            matrix[j] = zeroify(matrix[i], matrix[j], i)
-            print(f"Iteration #{iteration}: \n{matrix}\n")
-            iteration += 1
+            for j in range(i+1, len(matrix)):
+                matrix[j] = zeroify(matrix[i], matrix[j], i)
+                print(f"Iteration #{iteration}: \n{matrix}\n")
+                iteration += 1
 
-    os.system("pause")
+        # iteration 7 - 9
+        for i in range(len(matrix) - 1, 0, -1):
+            for j in range(i-1, -1, -1):
+                matrix[j] = zeroify(matrix[i], matrix[j], i)
+                print(f"Iteration #{iteration}: \n{matrix}\n")
+                iteration += 1
+
+        resp = input("Terminate program? (\"y\" if yes)> ")
+        if resp == "y":
+            running = False
+
+if __name__ == "__main__":
+    main()
